5fe96fe7c43b282c48ea2c3a1c3d3345a80864c8
Grégory Paul <me@gpaul.me>
2025-07-26 15:55:06 +0200

    chore: deployment preparation

--- src/index.tsx
+++ src/index.tsx
@@ -3,15 +3,16 @@ import { BrowserRouter, Routes, Route } from 'react-router-dom'
 import { createRoot } from 'react-dom/client'
 import App from './App.tsx'
 import HomePage from './pages/HomePage.tsx'
-import settings from './settings.json'
 import EditorPage from './pages/EditorPage.tsx'
 import ListPage from './pages/ListPage.tsx'
 
 if ('serviceWorker' in navigator)
-  navigator.serviceWorker.register(`${settings.baseUrl}sw.js`)
+  navigator.serviceWorker.register('/syncpad/sw.js')
 
 createRoot(document.getElementById('root')!).render(
-  <BrowserRouter basename={settings.baseUrl}>
+  <BrowserRouter
+    basename={process.env.NODE_ENV === 'production' ? '/syncpad' : ''}
+  >
     <Routes>
       <Route path="/" element={<App />}>
         <Route index element={<HomePage />} />
--- vite.config.ts
+++ vite.config.ts
@@ -3,6 +3,16 @@ import react from '@vitejs/plugin-react'
 import vitePluginSvgr from 'vite-plugin-svgr'
 
 // https://vite.dev/config/
-export default defineConfig({
-  plugins: [react(), vitePluginSvgr()],
+export default defineConfig(() => {
+  return {
+    build: {
+      sourcemap: true,
+    },
+    base: './',
+    plugins: [react(), vitePluginSvgr()],
+    test: {
+      include: ['**/*.test.js'],
+      globals: true,
+    },
+  }
 })
