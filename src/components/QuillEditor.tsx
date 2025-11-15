833a06dccc54b59177bee3e8a747a6faf7219a7e
Grégory Paul <me@gpaul.me>
2025-11-15 15:39:58 +0100

    feat: upload image by URL

--- src/components/QuillEditor.tsx
+++ src/components/QuillEditor.tsx
@@ -13,27 +13,48 @@ const QuillEditor: React.FC<QuillEditorProps> = ({ yText }) => {
   const containerRef = useRef(null)
   const quillRef = useRef<Quill>(null)
 
+  const imageHandler = () => {
+    if (!quillRef?.current) return
+    const range = quillRef.current.getSelection()
+    if (!range) return
+    const value = prompt('Inert the image url here', 'https://...')
+    if (value) {
+      quillRef.current.insertEmbed(
+        range.index,
+        'image',
+        value,
+        Quill.sources.USER
+      )
+    }
+  }
+
   useEffect(() => {
     if (!containerRef.current || quillRef.current) return
     quillRef.current = new Quill(containerRef.current, {
       modules: {
-        toolbar: [
-          [
-            { header: [false, 1, 2, 3, 4] },
-            { size: ['small', false, 'large'] },
+        toolbar: {
+          container: [
+            [
+              { header: [false, 1, 2, 3, 4] },
+              { size: ['small', false, 'large'] },
+            ],
+            [{ align: [] }, { color: [] }, { background: [] }],
+            [
+              'bold',
+              'italic',
+              'underline',
+              'strike',
+              'blockquote',
+              'code-block',
+              'link',
+              'image',
+            ],
+            [{ list: 'bullet' }, { list: 'ordered' }, { list: 'check' }],
           ],
-          [{ align: [] }, { color: [] }, { background: [] }],
-          [
-            'bold',
-            'italic',
-            'underline',
-            'strike',
-            'blockquote',
-            'code-block',
-            'link',
-          ],
-          [{ list: 'bullet' }, { list: 'ordered' }, { list: 'check' }],
-        ],
+          handlers: {
+            image: imageHandler,
+          },
+        },
         history: {
           userOnly: true, // Local undo shouldn't undo changes from remote users
         },
